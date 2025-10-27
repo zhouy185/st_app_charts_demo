7f462e32ef2ec0649fd53aaa33a228f488a5bff2
Yun Zhou <76829682+zhouy185@users.noreply.github.com>
2025-10-26 23:07:14 -0400

    Add Medals Explorer app using Streamlit and Plotly

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+st.set_page_config(page_title="Medals Explorer", layout="wide")
+st.title("Medals Explorer (Plotly + Streamlit)")
+
+medal = st.selectbox("Medal type", ["gold", "silver", "bronze"])
+show_bar = st.checkbox("Show Bar Chart", value=True)
+show_pie = st.checkbox("Show Pie Chart", value=True)
+
+
+col1, col2 = st.columns(2)
+
+
+df = px.data.medals_wide()
+
+if show_bar:
+    fig_bar = px.bar(
+        plot_df, 
+        x="nation", 
+        y=f"{medal}", 
+        title=f"Medals count({medal}})"
+        )
+    fig_bar.update_layout(title_x=0.5, yaxis_title="Count", xaxis_title="Nation", height=500)
+    col1.plotly_chart(fig_bar, use_container_width=True)
+
+if show_pie:
+    fig_pie = px.pie(plot_df, names="nation", values=f"{medal}",
+                     title=f"Medal Share ({medal}})")
+    fig_pie.update_traces(textinfo="percent+label")
+    fig_pie.update_layout(title_x=0.5, height=500)
+    col2.plotly_chart(fig_pie, use_container_width=True)
